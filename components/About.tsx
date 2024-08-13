2d250e1fbf5adf08489c5716c71130bd0c52549b
hilman <wlfuu3lqlaagp5x7mk6q0gf90xajwt@bots.bitbucket.org>
2024-08-13 23:53:37 +0700

    Update about

--- components/About.tsx
+++ components/About.tsx
@@ -28,13 +28,13 @@ const About = (props: Props) => {
         }}
         whileInView={{
           x: 0,
-          opacity: 1
+          opacity: 0.5
         }}
         viewport={{
           once: true
         }}
-        src="/assets/images/imgAbout.jpg"
-        className="opacity-50 -mb-20 mt-10 md:mb-0 flex-shrink-0 w-56 h-56 rounded-full object-cover md:rounded-lg md:w-64 md:h-95 xl:w-[500px] xl:h-[600px]"
+        src="/assets/images/code.jpg"
+        className="-mb-20 mt-10 md:mb-0 flex-shrink-0 w-56 h-56 rounded-full object-cover md:rounded-lg md:w-64 md:h-95 xl:w-[500px] xl:h-[600px]"
         />
 
         <div className="space-y-10 px-0 md:px-10">
